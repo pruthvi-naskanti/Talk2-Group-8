82347f8a12bde57083db4e4f5b1cc5f71aec233a
vishal Reddy <vishalreddy0507@gmail.com>
2021-02-12 10:22:54 -0600

    updated main.js and also the index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,16 @@
-
+let Color1=document.getElementById("c1");
+let Color2=document.getElementById("c2")
+function main(){
+    console.log("Main function has started")
+}
+window.addEventListener('load', main);
+Color1.addEventListener('touch',onClickColor1);
+Color1.addEventListener('click',onClickColor1);
+Color2.addEventListener('touch',onClickColor2);
+Color2.addEventListener('click',onClickColor2);
 function onClickColor1() {
     {
+        
         document.getElementById("c1").innerHTML = "The color displayed is light Grey.";       
         document.getElementById("c1").style.backgroundColor = "#71a832";
         let utterance = new SpeechSynthesisUtterance("The color displayed is light Grey.");
@@ -10,9 +20,9 @@ function onClickColor1() {
 }
 function onClickColor2() {
    {
+   
         document.getElementById("c2").innerHTML = "The color displayed is blue color.";
         document.getElementById("c2").style.backgroundColor  = "#6432a8";
-
         let utterance = new SpeechSynthesisUtterance("The color displayed is blue color.");
         speechSynthesis.speak(utterance);
     };
